7a15bb477a0ed8753a08f056e72ba4ee9a5f4eac
krosenk729 <krosenk729@gmail.com>
2018-05-06 07:59:06 -0400

    server routes matching with client api requests

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -28,19 +28,15 @@ export default {
   getEpisodes: function() {
     return axios.get('/api/episode');
   },
-  // Gets the episode with the given id
   getSingleEpisode: function(id) {
     return axios.get('/api/episode/' + id);
   },
-  // Gets the episode with the given eid
   getSingleEID: function(eid) {
     return axios.get('/api/episode/eid/' + eid);
   },
-  // Deletes the episode with the given id
   deletePodcast: function(id) {
     return axios.delete('/api/episode/' + id);
   },
-  // Saves a scraped podcast to the database
   savePodcast: function(episodeData) {
     return axios.post('/api/episode', episodeData);
   }
