49b3c5388cef00a2f872b92f537f3b5dffcebc8f
krosenk729 <krosenk729@gmail.com>
2018-05-06 07:58:41 -0400

    server routes matching with client api requests

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,30 +1,48 @@
 import axios from 'axios';
 
 export default {
-  
-  // Gets all scraped data
+
+  // **************************************************************/
+  // Scraped Data
+
   getScrapes: function() {
     return axios.get('/api/scrapes');
   },
-  // Gets all podcasts
+  
+  // **************************************************************/
+  // Podcasts 
+
   getPodcasts: function() {
     return axios.get('/api/podcast');
   },
-  // Gets the podcast with the given id
-  getSinglePodcast: function(id) {
-    return axios.get('/api/podcast/' + id);
-  },
-  // Gets the podcast with the given id
-  getSingleEpisode: function(eid) {
-    return axios.get('/api/podcast/eid/' + eid);
-  },
-  // Deletes the podcast with the given id
   deletePodcast: function(id) {
     return axios.delete('/api/podcast/' + id);
   },
-  // Saves a scraped podcast to the database
   savePodcast: function(podcastData) {
     return axios.post('/api/podcast', podcastData);
+  },
+  
+  // **************************************************************/
+  // Saved Episodes
+
+  getEpisodes: function() {
+    return axios.get('/api/episode');
+  },
+  // Gets the episode with the given id
+  getSingleEpisode: function(id) {
+    return axios.get('/api/episode/' + id);
+  },
+  // Gets the episode with the given eid
+  getSingleEID: function(eid) {
+    return axios.get('/api/episode/eid/' + eid);
+  },
+  // Deletes the episode with the given id
+  deletePodcast: function(id) {
+    return axios.delete('/api/episode/' + id);
+  },
+  // Saves a scraped podcast to the database
+  savePodcast: function(episodeData) {
+    return axios.post('/api/episode', episodeData);
   }
 
 };
