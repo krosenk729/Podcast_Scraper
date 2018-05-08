b47868bf20208e9a9e750b78f6e8899e6877cca1
krosenk729 <krosenk729@gmail.com>
2018-05-08 06:32:31 -0400

    switching to using db for scrapes -- deleting old files

--- routes/api/scrapes.js
+++ routes/api/scrapes.js
@@ -1,15 +1,9 @@
 const router = require('express').Router();
-const scrapesController = require('../../controllers/scrapesController');
 const scrapeController = require('../../controllers/scrapeController');
 
 // =============================================================
 // Matches with '/api/scrapes'
 router.route('/')
-  .get(scrapesController.scrapeAll);
-
-
-// Matches with '/api/scrapestest'
-router.route('/test')
   .get(scrapeController.scrapeAll);
 
 // =============================================================
