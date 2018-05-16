eadddebb9d32e56ffdfe149f0d6fb122247c7eb0
krosenk729 <krosenk729@gmail.com>
2018-05-16 10:07:20 -0400

    url scrapes showing -- todo: card footer

--- client/src/pages/Podcasts/AddPodcast.js
+++ client/src/pages/Podcasts/AddPodcast.js
@@ -25,17 +25,17 @@ class AddPodcast extends Component {
 
   handleChange = event =>{
     const newState = {};
-    const { name, link } = event.target;
-    newState[name] = link;
+    const { name, value } = event.target;
+    newState[name] = value;
 
-    if( link && Util.checkStitchUrl( link)){
+    if( value && Util.checkStitchUrl( value)){
       newState.linkIsValid = true;
-      this.handleScrape(link);
+      this.handleScrape(value);
     } else {
       newState.linkIsValid = false;
       newState.tempPod = {};
     }
-
+    console.log('newState', newState);
     this.setState(newState);
   }
 
@@ -45,19 +45,34 @@ class AddPodcast extends Component {
   } 
 
   handleSubmit = data =>{
-    API.getSingleScrape(this.state.link)
-    .then(scrape =>  this.setState({tempPod: scrape.data}));
+    // API.getSingleScrape(this.state.link)
+    // .then(scrape =>  this.setState({tempPod: scrape.data}));
   }
 
   // **************************************************************/
   // Render
 
   render() {
+    const renderTempPod = () => {
+      if(this.state.linkIsValid && this.state.tempPod){
+        return (
+          <React.Fragment>
+          <h2>{this.state.tempPod.podcast}</h2>
+          <a href={this.state.link} target="_blank" rel="noopener noreferrer">{this.state.link}</a>
+          </React.Fragment>
+          )
+      } else {
+        return (
+           <a href="https://www.stitcher.com/stitcher-list/" target="_blank" rel="noopener noreferrer">Find a new Podcast from Stitcher</a>
+          )
+      }
+    }
+
     return (
         <Card>
         <CardBody>
         <h3>Add a Podcast</h3>
-        <p>Is there a podcast from <a href="https://www.stitcher.com/stitcher-list/" target="_blank">Stitcher</a> that you want to add?</p>
+        <p>Is there a podcast from <a href="https://www.stitcher.com/stitcher-list/" target="_blank" rel="noopener noreferrer">Stitcher</a> that you want to add?</p>
         <label htmlFor="link">Stitcher Link:</label>
         <input 
         onChange={this.handleChange} 
@@ -68,12 +83,7 @@ class AddPodcast extends Component {
 
         <button className="btn" onClick={this.handleSubmit}>Add Podcast</button>
         </CardBody>
-        <CardFooter>
-        <a href={this.state.link} target="_blank" rel="noopener noreferrer">{this.state.link}</a>
-        {this.state.tempPod ? 
-          <p>{this.state.tempPod.podcast}</p> : 
-          null}
-        </CardFooter>
+        <CardFooter>{renderTempPod()}</CardFooter>
         </Card>
     );
   }
